bf27a86911b723e5a8006b075acfb8f79ed9c8d1
laurens de vries <leh.devries@gmail.com>
2017-04-20 17:21:27 +0200

    need to finish stuff

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
-# gem 'simple_form'
+gem 'simple_form'
 gem 'autoprefixer-rails'
 
 # Use Capistrano for deployment
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+ingredients = open(url).read
+
+ingredients_hash = JSON.parse(ingredients)
+
+ingredients = ingredients_hash['drinks'].map { |h| h['strIngredient1'] }
+
+cocktail_names = ["margarita", "white russian", "sex on the beach", "AppleJack",
+                  "Sunny Holiday Punch", "Lemon Drop", "After Dinner Cocktail",
+                  "Patties Pis", "Kevins love potion", "A drink byJoe", "Robinacolada"]
+
+ingredients.each do |ingredient|
+  Ingredient.create(name: ingredient)
+end
+
+cocktail_names.each do |name|
+  cocktail = Cocktail.new(name: name)
+  cocktail.ingredients = Ingredient.all.sample(5)
+  cocktail.save
+end
